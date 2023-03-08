0895a6369a3c9ec6e4a4ec49616b3ef524a9fe97
Ritika Laddha <69119436+Ritika54@users.noreply.github.com>
2023-03-08 11:48:48 +0530

    Update UITest.java

--- src/test/java/mavenforjenkins/UITest.java
+++ src/test/java/mavenforjenkins/UITest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Parameters;
+import org.testng.annotations.Optional;
 import org.testng.annotations.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
