15b8bc7f140e7f59f8d7bc0e815ea118df0456ef
Ritika Laddha <69119436+Ritika54@users.noreply.github.com>
2023-03-08 11:27:44 +0530

    Update UITest.java

--- src/test/java/mavenforjenkins/UITest.java
+++ src/test/java/mavenforjenkins/UITest.java
@@ -12,7 +12,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class UITest 
 {
 
-	@Parameters("Browser")
+	@Parameters("Browser", "url")
 	@Test
 	public void startBrowser(String browserName)
 	{
